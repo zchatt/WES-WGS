78a236f6a88813c8a8496872de963882cf72ba80
zchatt <30888259+zchatt@users.noreply.github.com>
2017-11-28 11:05:50 +1100

    Add files via upload

---
+++ annovar.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+ 
+### Author: Zac Chatterton
+### Date: 2017/11/27
+
+# Modules
+module load annovar/20170716
+
+if [ -z $1 ]; then
+    echo "Need to submit name of folder" && exit
+fi
+
+if [ -z $2 ]; then
+    echo "Need to path to folder to process" && exit
+fi
+
+## Define project folder
+inDir="${2}"
+echo "inDir= $inDir"
+
+outDir=${inDir}"/results"
+echo $outDir && mkdir -p $outDir
+
+
+## Annotations
+indexDir="/project/RDS-SMS-FFbigdata-RW/local_lib/genomes/hg38_broad_bundle/"
+echo "indexDir is $indexDir"
+
+genomeDir=${indexDir}"Homo_sapiens_assembly38.fasta"
+echo "genomeDir is $genomeDir"
+
+hg38_annotation_dir="/project/RDS-SMS-FFbigdata-RW/local_lib/genomes/hg38/annotation/humandb/"
+echo "hg38_annotation_dir is $hg38_annotation_dir"
+
+cohort_vcf=${outDir}/${1}.g.vcf
+echo "cohort_vcf is $cohort_vcf"
+
+##########################################################################################################################
+##########################################################################################################################
+# 1. Functional Annotioan of Variants using ANNOVAR
+##########################################################################################################################
+##########################################################################################################################
+
+if [ ! -f $outDir/${1}.g.vcf ];
+then
+  echo "Running ANNOVAR"
+    
+  echo "Running table_annovar.pl $cohort_vcf $hg38_annotation_dir \
+     -buildver hg38 \
+     -out $final_vcf_name \
+     -remove 
+     -protocol refGene,cytoBand,exac03,exac03nontcga,exac03nonpsych,avsnp147,dbnsfp33a,dbscsnv11,cosmic70,esp6500siv2_ea,esp6500siv2_aa,esp6500siv2_all,gnomad_exome,gnomad_genome,AFR.sites.2015_08,ALL.sites.2015_08,AMR.sites.2015_08,mcap,revel,clinvar_20170130 \
+     -operation gx,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f \
+     -nastring . 
+     -vcfinput"
+  echo "[TIME: ANNOVAR]"
+  time table_annovar.pl $outDir/${1}.g.vcf $hg38_annotation_dir \
+     -buildver hg38 \
+     -out ${outDir}/${1}_annovar \
+     -remove 
+     -protocol refGene,cytoBand,exac03,exac03nontcga,exac03nonpsych,avsnp147,dbnsfp33a,dbscsnv11,cosmic70,esp6500siv2_ea,esp6500siv2_aa,esp6500siv2_all,gnomad_exome,gnomad_genome,AFR.sites.2015_08,ALL.sites.2015_08,AMR.sites.2015_08,mcap,revel,clinvar_20170130 \
+     -operation gx,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f \
+     -nastring . 
+     -vcfinput
+ else
+  echo "Found" $outDir/${1}.g.vcf
+fi
---
+++ pjob_bam.sh
@@ -0,0 +1,287 @@
+#!/bin/sh
+
+TRANSFERQUEUE="dtq"
+RSYNC=/usr/local/bin/rsync
+RSYNCFLAGS=-avx
+
+# Note that rsync always verifies that each transferred  file  was
+# correctly  reconstructed  on  the  receiving  side by checking a
+# whole-file checksum that is generated  as  the  file  is  trans-
+# ferred. The rsync --checksum option is a method whereby the files
+# choosen to be transfered are judged by checksum differences on
+# the receiving and sending end.
+
+#default walltime for transfer job.
+SKIPREADTEST=""
+WALLTIME="200:00:00"
+CMEM="16gb"
+CNCPUS="2"
+DEPEND="afterok"
+PROJECT=""
+NAME="pjob"
+QSUBEXTRAFLAGS=""
+
+MEM="64gb"
+NCPUS="8"
+
+
+# usage dt-script --from=/dir --to=/dir --after=pbsscript
+
+
+function usage {
+cat << _HERE
+usage $1 --from|-f <source> --to|-t <dest> --file|-f <folderlist> options
+from, to and project must be specified.
+where options are:
+--from|-f = the source of the data
+--to|-t = the destination of the data
+
+--skip|-notest - skip testing of readable source. Useful if called from a node and /rds is the source which is not available on the calling node.
+
+--walltime|-w <walltime> (default $WALLTIME)
+--ncpus|-ncpus <ncpus> (default $NCPUS)
+--mem|-m <mem> (default $MEM)
+
+-N|-n|--name <name> = set the copyjob name. (default "pjob");
+
+--rflags|-rf <rsync extra flags> - any extra rsync flags you may require.
+
+--job|-j <file containing list of folder name> - the list of folder names to run after the copy.
+
+_HERE
+
+exit 0
+}
+
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+	--help | -h)
+		usage $0
+		shift
+		;;
+	--skip | -notest)
+		shift
+		SKIPREADTEST="yes"
+		;;
+	--log | -l)
+		shift
+		LOG="$1"
+		shift
+		;;
+	--name | -n | -N)
+		shift
+		NAME="$1"
+		shift
+		;;
+	--from | -f)
+		shift
+		from="$1"
+		shift
+		;;
+	--to | -t)
+		shift
+		to="$1"
+		shift
+		;;
+	--rflags | -rf)
+		shift
+		rsyncflags="$1"
+		shift
+		;;
+	--qflags | -qf)
+		shift
+		QSUBEXTRAFLAGS="$1"
+		shift
+		;;
+	--job | -j)
+		shift
+		job="$1"
+		shift
+		;;
+	--depend | -d)
+		shift
+		DEPEND="$1"
+		shift
+		;;
+	--walltime | -w)
+		shift
+		WALLTIME="$1"
+		shift
+		;;
+	--project | -P)
+		shift
+		PROJECT="$1"
+		shift
+		;;
+	--mem | -m)
+		shift
+		MEM="$1"
+		shift
+		;;
+	--ncpus | -ncpus)
+		shift
+		NCPUS="$1"
+		shift
+		;;
+	*)
+		echo Unknown arg $1
+		usage $0
+		exit 1
+		break
+		;;
+	esac
+done
+
+
+function checkjobflags {
+	# Check if there's a -I - interactive makes no sense in this context.
+	iflag=$(echo $1 | egrep -w -e -I)
+	if [ ! -z "$iflag" ]
+	then
+		echo "Interactive flag -I makes no sense in this context"
+		exit 1
+	fi
+}
+function checkpath {
+	fpath=$1
+	isfrom=$2
+
+	if [ -z $SKIPREADTEST ]
+	then
+		f=${fpath:0:1}
+		if [ "$f" != "/" ]
+		then
+			echo path $fpath should be a full pathname.
+			exit 1
+		fi
+		if [ ! -z $isfrom ]
+		then
+			# This deals with wildcards. ie from="/bla/blo/*"
+			for p in $fpath
+			do
+				if [ ! -r $p ]
+				then
+					echo from path $p is not readable.
+					exit 1
+				fi
+			done
+		fi
+	fi
+}
+
+if [ -z "$from" ]
+then
+	echo A source \(--from or -f\) must be specified.
+	exit 1
+fi
+if [ -z "$to" ]
+then
+	echo A destination \(--to or -t\) must be specified.
+	exit 1
+fi
+if [ -z "$job" ]
+then
+	echo A file list containing folder names \(--file or -f\) must be specified.
+	exit 1
+fi
+
+
+
+QCOPYFLAGS="-q $TRANSFERQUEUE -P FFbigdata $QSUBEXTRAFLAGS"
+
+# Check from and to as being full path names.
+checkpath "$from" yes
+checkpath "$to"
+
+#copyDirs=""
+#if [ -z "$LOG" ]
+#then
+#	while read folder           
+#	do           
+#		copyDirs=$copyDirs" $RSYNC $RSYNCFLAGS $rsyncflags $from/$folder $to;"           
+#	done <$job
+#	copy_from_sync_jobid=$(printf "#PBS -l select=1:ncpus=$NCPUS:mem=$MEM,walltime=$WALLTIME
+#	#PBS -N $NAME"_start"
+#	cd \$PBS_O_WORKDIR
+#	time /bin/sh -c '$copyDirs'" | qsub $QCOPYFLAGS)
+#else
+#	while read folder           
+#	do           
+#		copyDirs=$copyDirs" $RSYNC $RSYNCFLAGS $rsyncflags $from/$folder $to;"           
+#	done <$job
+#	copy_from_sync_jobid=$(printf "#PBS -l select=1:ncpus=$NCPUS:mem=$MEM,walltime=$WALLTIME
+#	#PBS -N $NAME"_start"
+#	cd \$PBS_O_WORKDIR
+#	time /bin/sh -c '$copyDirs'" | qsub $QCOPYFLAGS)
+#
+#fi
+
+#if [ -z $copy_from_sync_jobid ]
+#then
+#	echo Submit of copy job failed.
+#	exit 1
+#fi
+
+#copy_from_sync_jobid=$(echo $copy_from_sync_jobid | cut -d'.' -f 1)
+
+# If there was a pbs job file specified, then submit it such that it depends
+# on the copy job (usually afterok, but that can be changed with -d)
+
+QFLAGS="-P FFbigdata -j oe $QSUBEXTRAFLAGS"
+#QFLAGS="-P FFbigdata $QSUBEXTRAFLAGS"
+
+echo "job submitted..."
+while read folder
+do          
+	#check if folder or file
+	echo "checkpath: " $from/$folder
+	finalto=$to
+	if [ -d "$from/$folder" ] 
+	then
+		#rsync from folder to folder
+		echo "Found folder $from/$folder"
+		copy_from_sync_jobid=$(printf "#PBS -l select=1:ncpus=$CNCPUS:mem=$CMEM,walltime=$WALLTIME
+		#PBS -N $NAME"_"$folder"_start"
+		time $RSYNC $RSYNCFLAGS $rsyncflags $from/$folder $to" | qsub $QCOPYFLAGS)
+		#update $to path with folder
+		finalto=$to/$folder
+		copy_from_sync_jobid=$(echo $copy_from_sync_jobid | cut -d'.' -f 1)
+	else
+		#rsync from file to folder
+		echo "No folder found $from/$folder"
+		#reset ${to} path to new folder path
+		finalto=$to/${folder%.*}
+		copy_from_sync_jobid=$(printf "#PBS -l select=1:ncpus=$CNCPUS:mem=$CMEM,walltime=$WALLTIME
+		#PBS -N $NAME"_"$folder"_start"
+		mkdir $finalto; time $RSYNC $RSYNCFLAGS $rsyncflags $from/$folder $finalto" | qsub $QCOPYFLAGS)
+		copy_from_sync_jobid=$(echo $copy_from_sync_jobid | cut -d'.' -f 1)
+		echo $copy_from_sync_jobid", sync from $from/$folder to $to"
+	fi
+
+	#Run gatk
+	if [ ! -z "$copy_from_sync_jobid" ]
+	then
+		gatk_jobid=$(printf "#PBS -l select=1:ncpus=$NCPUS:mem=$MEM,walltime=$WALLTIME
+		#PBS -N $NAME"_"$folder"_gatk"
+		$HOME/scripts/bam2fastq.sh $folder ${to} $NCPUS" | qsub -W depend=$DEPEND:$copy_from_sync_jobid $QFLAGS)
+		gatk_jobid=$(echo $gatk_jobid | cut -d'.' -f 1)
+		echo $gatk_jobid", run gatk with bam2fastq.sh"
+	fi
+
+	#rsync result back to folder
+	if [ ! -z "$gatk_jobid" ]
+	then
+		#folder=$(basename $finalto)
+		copy_back_to_jobid=$(printf "#PBS -l select=1:ncpus=$CNCPUS:mem=$CMEM,walltime=$WALLTIME
+		#PBS -N $NAME"_"${folder%.*}"_results"
+		$RSYNC $RSYNCFLAGS $rsyncflags $finalto $from" | qsub -W depend=$DEPEND:$copy_from_sync_jobid:$gatk_jobid $QCOPYFLAGS)
+		copy_back_to_jobid=$(echo $copy_back_to_jobid | cut -d'.' -f 1)
+		echo "results: $RSYNC $RSYNCFLAGS $rsyncflags $to $from"
+		echo ${to}" jobid: "$copy_from_sync_jobid $gatk_jobid $copy_back_to_jobid
+	fi
+done <$job
+
+echo "done"
+exit 0
