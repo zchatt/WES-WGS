3b0e433d01f5e2348ff9b72f86e22356dcf8f444
Bill Lee <lee.minwee@gmail.com>
2018-01-04 09:57:22 +1100

    Update annovar to avsnp150 and added threading

--- annovar.sh
+++ annovar.sh
@@ -21,6 +21,8 @@ echo "inDir= $inDir"
 outDir=${inDir}"/annovar"
 echo $outDir && mkdir -p $outDir
 
+numcores=${3}
+echo "numcores= $numcores"
 
 ## Annotations
 indexDir="/project/RDS-SMS-FFbigdata-RW/local_lib/genomes/hg38_broad_bundle/"
@@ -32,7 +34,7 @@ echo "genomeDir is $genomeDir"
 hg38_annotation_dir="/project/RDS-SMS-FFbigdata-RW/local_lib/genomes/hg38/annotation/humandb/"
 echo "hg38_annotation_dir is $hg38_annotation_dir"
 
-cohort_vcf=${2}/${1}.g.vcf
+cohort_vcf=${2}/results/${1}.g.vcf
 echo "cohort_vcf is $cohort_vcf"
 
 ##########################################################################################################################
@@ -49,18 +51,20 @@ then
      -buildver hg38 \
      -out ${outDir}/${1}_annovar \
      -remove \
-     -protocol refGene,cytoBand,exac03,exac03nontcga,exac03nonpsych,avsnp147,dbnsfp33a,dbnsfp31a_interpro,dbscsnv11,cosmic70,esp6500siv2_ea,esp6500siv2_aa,esp6500siv2_all,gnomad_exome,gnomad_genome,AFR.sites.2015_08,ALL.sites.2015_08,AMR.sites.2015_08,EUR.sites.2015_08,mcap,revel,clinvar_20170130 \
+     -protocol refGene,cytoBand,exac03,exac03nontcga,exac03nonpsych,avsnp150,dbnsfp33a,dbnsfp31a_interpro,dbscsnv11,cosmic70,esp6500siv2_ea,esp6500siv2_aa,esp6500siv2_all,gnomad_exome,gnomad_genome,AFR.sites.2015_08,ALL.sites.2015_08,AMR.sites.2015_08,EUR.sites.2015_08,mcap,revel,clinvar_20170130 \
      -operation gx,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f \
      -nastring . \
+     --thread $numcores \
      -vcfinput"
   echo "[TIME: ANNOVAR]"
   time table_annovar.pl $cohort_vcf $hg38_annotation_dir \
      -buildver hg38 \
      -out ${outDir}/${1}_annovar \
      -remove \
-     -protocol refGene,cytoBand,exac03,exac03nontcga,exac03nonpsych,avsnp147,dbnsfp33a,dbnsfp31a_interpro,dbscsnv11,cosmic70,esp6500siv2_ea,esp6500siv2_aa,esp6500siv2_all,gnomad_exome,gnomad_genome,AFR.sites.2015_08,ALL.sites.2015_08,AMR.sites.2015_08,EUR.sites.2015_08,mcap,revel,clinvar_20170130 \
+     -protocol refGene,cytoBand,exac03,exac03nontcga,exac03nonpsych,avsnp150,dbnsfp33a,dbnsfp31a_interpro,dbscsnv11,cosmic70,esp6500siv2_ea,esp6500siv2_aa,esp6500siv2_all,gnomad_exome,gnomad_genome,AFR.sites.2015_08,ALL.sites.2015_08,AMR.sites.2015_08,EUR.sites.2015_08,mcap,revel,clinvar_20170130 \
      -operation gx,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f \
      -nastring . \
+     --thread $numcores \
      -vcfinput
  else
   echo "Found" $outDir/${1}.g.vcf
